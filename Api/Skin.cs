e773a191467a6534e176ca1981913c8cff58f336
Stanilav Hacker <info@dbsys.cz>
2014-08-18 22:01:10 +0200

    Api skin

---
+++ Api/Skin.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoPilot.Core.Api
+{
+    public class Skin
+    {
+        /// <summary>
+        /// Background
+        /// </summary>
+        public String Background { get; set;  }
+
+        /// <summary>
+        /// Foreground
+        /// </summary>
+        public String Foreground { get; set; }
+    }
+}
