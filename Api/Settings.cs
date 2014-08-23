014fb7875ce0a6490c4754c218990ae83f284dab
Stanilav Hacker <info@dbsys.cz>
2014-08-23 14:13:05 +0200

    Uprava api, pridani settings

---
+++ Api/Settings.cs
@@ -0,0 +1,39 @@
+﻿using CoPilot.Core.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoPilot.Core.Api
+{
+    public class Settings
+    {
+        /// <summary>
+        /// Currency
+        /// </summary>
+        public String Currency { get; set; }
+
+        /// <summary>
+        /// Distance
+        /// </summary>
+        public String Distance { get; set; }
+
+        /// <summary>
+        /// Consumption
+        /// </summary>
+        public String Consumption { get; set; }
+
+        public Double SummaryFuelPrice { get; set; }
+        public Double SummaryRepairPrice { get; set; }
+        public Double Liters { get; set; }
+
+        public Int32 Repairs { get; set; }
+        public Int32 Maintenances { get; set; }
+        public Int32 Fills { get; set; }
+        public Int32 Videos { get; set; }
+        public Int32 Pictures { get; set; }
+        public Int32 Paths { get; set; }
+
+    }
+}
